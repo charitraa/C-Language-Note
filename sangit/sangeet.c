f5739c4baf017ff3814f8c0cc38c7c60f8d7a685
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-09-08 16:50:30 +0545

    new

---
+++ sangit/sangeet.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+    // this is my first c progamming
+    char marks[50];
+    printf("Enter your name:");
+    gets(marks);
+    printf("your name is %s", marks);
+    return 0;
+}
